25caf88e9c9b97c0ff3b76bb63216585f2eba5c1
cdilday <cdilday@ucsc.edu>
2015-01-22 15:04:59 -0800

    Quick n Dirty Back & forth AI implemented.

--- AmeliaBlume/Source/AmeliaBlume/Deer.cpp
+++ AmeliaBlume/Source/AmeliaBlume/Deer.cpp
@@ -3,12 +3,62 @@
 #include "AmeliaBlume.h"
 #include "Deer.h"
 
+ADeer::ADeer(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	//set up facing the proper direction, -1 for left, 1 for right
+	faceDirection = -1;
 
+	//recentlyRotated = true;
+	//rotationCooldown = 60;
+
+	//this would lock it to the location in the editor but it's not working properly
+	//lockedAxisValue = GetActorLocation().X;
+	//This locks it to the current location of the plane axis
+	lockedAxisValue = 1210.0;
+}
 
 void ADeer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	//simple AI movement, will be controlled by states later
+	MoveRight(faceDirection * 0.5);
 
 
+	//Simple Back & forth. If the velocity is ~0, it's collided and need to turn around
+	if ( !recentlyRotated && GetVelocity().Y <= 0.1 && GetVelocity().Y >= -0.1)
+	{
+		faceDirection *= -1;
+		recentlyRotated = true;
+		rotationCooldown = 60;
+	}
+
+	if (rotationCooldown > 0)
+	{
+		rotationCooldown--;
+		FRotator quickRotate = GetActorRotation();
+		quickRotate.Yaw += 3.0;
+		SetActorRotation(quickRotate);
+	}
+	else
+		recentlyRotated = false;
+
+	//Some simple code to keep character locked to a plane
+	FVector currLoc = GetActorLocation();
+	FVector newLoc = FVector(lockedAxisValue, currLoc.Y, currLoc.Z);
+	SetActorLocation(newLoc);
 }
 
+void ADeer::MoveRight(float Value)
+{
+	// add movement in that direction
+	AddMovementInput(FVector(0.f, -1.f, 0.f), Value);
+	UE_LOG(LogTemp, Warning, TEXT("Deer Facing right = %d"), isFacingRight);
+	if (Value < 0)
+		isFacingRight = false;
+	else if (Value > 0)
+		isFacingRight = true;
+	else
+		isFacingRight = isFacingRight;
+}
--- AmeliaBlume/Source/AmeliaBlume/Deer.h
+++ AmeliaBlume/Source/AmeliaBlume/Deer.h
@@ -13,8 +13,27 @@ class AMELIABLUME_API ADeer : public ACharacter
 {
 	GENERATED_BODY()
 
+	//saves the initial value of the axis rather than memorizing a hard number
+	UPROPERTY(EditAnywhere)
+		float lockedAxisValue;
+
+	UPROPERTY(EditAnywhere)
+		bool isFacingRight;
+	
+	UPROPERTY(EditAnywhere)
+		int8 faceDirection;
+
+	UPROPERTY(EditAnywhere)
+		bool recentlyRotated;
+
+	UPROPERTY(EditAnywhere)
+		int32 rotationCooldown;
+
 	//override default tick function
 	virtual void Tick(float DeltaTime) OVERRIDE;
-	
+	virtual void MoveRight(float value);
+
+public:
+	ADeer(const FObjectInitializer& ObjectInitializer);
 	
 };
