7b96cc3bdde4495329a9174f7c423d96040b0e1b
Joe Wise <JosephWise93@gmail.com>
2015-01-26 02:25:08 -0800

    created platform classes

---
+++ AmeliaBlume/Source/AmeliaBlume/AmeliaPlatform.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "AmeliaPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMELIABLUME_API AAmeliaPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
